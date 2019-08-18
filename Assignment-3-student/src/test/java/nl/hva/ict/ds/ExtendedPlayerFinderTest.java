0df93bab938c56231707460588bbf27245a4810a
daanbouws1 <daanbouws1@gmail.com>
2019-08-18 16:10:07 +0200

    added extra tests

--- Assignment-3-student/src/test/java/nl/hva/ict/ds/ExtendedPlayerFinderTest.java
+++ Assignment-3-student/src/test/java/nl/hva/ict/ds/ExtendedPlayerFinderTest.java
@@ -3,7 +3,9 @@ package nl.hva.ict.ds;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.nio.charset.Charset;
 import java.util.List;
+import java.util.Random;
 
 import static junit.framework.Assert.assertTrue;
 import static junit.framework.TestCase.assertEquals;
@@ -51,4 +53,59 @@ public class ExtendedPlayerFinderTest extends HighScorePlayerFinderTest {
         assertEquals(1, albusDumbledores.size());
         assertTrue(albusses.get(0) == dumbledores.get(0) && dumbledores.get(0) == albusDumbledores.get(0));
     }
+
+    @Test
+    public void shouldBeAbleToRetrieveAllPlayers() {
+        highscores = new HighScorePlayerFinder(200);
+        Player[] testPlayers = new Player[100];
+        for (int i = 0; i < testPlayers.length; i++) {
+            String randomFirstname = getAlphaNumericString(10);
+            String randomLastname = getAlphaNumericString(11);
+            Player randomPlayer = new Player(randomFirstname, randomLastname, (long) Math.random() * 10000);
+            testPlayers[i] = randomPlayer;
+            highscores.add(randomPlayer);
+        }
+
+        for (Player player: testPlayers) {
+            assertEquals(player, highscores.findPlayer(player.getFirstName(), player.getLastName()).get(0));
+        }
+    }
+
+    @Test
+    public void shouldGetAllIdenticallyNamedPlayers() {
+        highscores = new HighScorePlayerFinder(3);
+
+        Player player1 = new Player("test","test",10);
+        Player player2 = new Player("test", "test", 10);
+        highscores.add(player1);
+        highscores.add(player2);
+
+        assertEquals(2, highscores.findPlayer("test", "test").size());
+    }
+
+    String getAlphaNumericString(int n)
+    {
+
+        // chose a Character random from this String
+        String AlphaNumericString = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvxyz";
+
+        // create StringBuffer size of AlphaNumericString
+        StringBuilder sb = new StringBuilder(n);
+
+        for (int i = 0; i < n; i++) {
+
+            // generate a random number between
+            // 0 to AlphaNumericString variable length
+            int index
+                    = (int)(AlphaNumericString.length()
+                    * Math.random());
+
+            // add Character one by one in end of sb
+            sb.append(AlphaNumericString
+                    .charAt(index));
+        }
+
+        return sb.toString();
+    }
+
 }
