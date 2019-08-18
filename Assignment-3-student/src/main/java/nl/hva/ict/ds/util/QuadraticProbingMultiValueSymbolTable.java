4dc47ca860a73882d445d3c73cff4e96a74aebbb
daanbouws1 <daanbouws1@gmail.com>
2019-08-18 16:09:19 +0200

    converted datastructure to Player[]

--- Assignment-3-student/src/main/java/nl/hva/ict/ds/util/QuadraticProbingMultiValueSymbolTable.java
+++ Assignment-3-student/src/main/java/nl/hva/ict/ds/util/QuadraticProbingMultiValueSymbolTable.java
@@ -3,37 +3,36 @@ package nl.hva.ict.ds.util;
 import nl.hva.ict.ds.Player;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class QuadraticProbingMultiValueSymbolTable implements MultiValueSymbolTable<String, Player> {
 
     private int size;
-    private List<Player> map;
+    private Player[] map;
     private int collision;
 
     public QuadraticProbingMultiValueSymbolTable(int arraySize) {
         this.size = arraySize;
-        this.map = new ArrayList<>();
-        for (int i = 0; i < arraySize; i++) {
-            this.map.add(null);
-        }
+        this.map = new Player[arraySize];
         this.collision = 0;
     }
 
     @Override
     public void put(String key, Player value) {
         int hash = Math.abs(key.hashCode() % size);
-        if (map.get(hash) == null) {
-            map.set(hash, value);
+        if (map[hash] == null) {
+            map[hash] = value;
         } else {
             this.collision++;
             int nextIndex = hash;
             for (int i = 0; i < size; i++) {
                 nextIndex = (int) ((nextIndex+Math.pow(i,2)) % size);
-                if (map.get(nextIndex) == null) {
-                    map.set(nextIndex, value);
+                if (map[nextIndex] == null) {
+                    map[nextIndex] = value;
                     break;
                 } else {
                     this.collision++;
@@ -44,7 +43,8 @@ public class QuadraticProbingMultiValueSymbolTable implements MultiValueSymbolTa
 
     @Override
     public List<Player> get(String key) {
-        return this.map.stream()
+        Stream<Player> stream = Arrays.stream(this.map);
+        return stream
                 .filter(player -> player != null && Objects.equals(player.getLastName(), key))
                 .collect(Collectors.toList());
     }
